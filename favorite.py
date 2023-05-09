b5db2581935a931b352656179af9c00eee7ca12e
Sajith Mohan Plavalappil <sajithmohan1991992@gmail.com>
2023-05-09 10:30:09 -0400

    csvfavourite python

--- favorite.py
+++ favorite.py
@@ -2,4 +2,9 @@ import csv
 with open("file.csv", "r" ) as file: # this just open the file and name as file  now in next we have to add to the library
     reader = csv.reader(file)
     for row in reader:
+<<<<<<< HEAD
         print(row[1])
+=======
+     favorite = row[1]
+     print(favorite)
+>>>>>>> a095eae (csvfavourite python)
