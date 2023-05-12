16fbe771e4c4efe62f52caab83a4e8a61e207bb1
Sajith Mohan Plavalappil <sajithmohan1991992@gmail.com>
2023-05-12 08:16:12 -0400

    reverse sorted.py

---
+++ sortsheetoutput.py
@@ -0,0 +1,12 @@
+import csv
+with open ("file.csv","r") as file:
+    reader = csv.DictReader(file)
+    counts = {}
+    for row in reader:
+      favorite = row["Segment"]
+      if favorite in counts:
+         counts[favorite] += 1
+      else:
+         counts[favorite] = 1
+for favorite in sorted(counts , reverse =True):
+   print(f"{favorite}:{counts[favorite]}")
