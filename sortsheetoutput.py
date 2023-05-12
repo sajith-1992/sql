0f177133edb4c1918181648b5433d35833818748
Sajith Mohan Plavalappil <sajithmohan1991992@gmail.com>
2023-05-12 10:22:25 -0400

    lambda arrgument in pyhton sort

--- sortsheetoutput.py
+++ sortsheetoutput.py
@@ -2,13 +2,13 @@ import csv
 with open ("file.csv","r") as file:
     reader = csv.DictReader(file)
     counts = {}
+    
     for row in reader:
       favorite = row["Segment"]
       if favorite in counts:
          counts[favorite] += 1
       else:
          counts[favorite] = 1
-def get_value(segment):
-   return counts[segment]
-for favorite in sorted(counts ,key =get_value, reverse =True):
+
+for favorite in sorted(counts ,key = lambda segment:counts[segment],reverse= True):# this wil sort on the value it has not the key value hear will be the number 
    print(f"{favorite}:{counts[favorite]}")
