7a5e53152bd1bb26e53e8545de22bf64c7f295ec
Sajith Mohan Plavalappil <sajithmohan1991992@gmail.com>
2023-05-21 11:55:11 -0400

    pythonsqlite data printing

--- sql_python.py
+++ sql_python.py
@@ -6,8 +6,12 @@ db = sqlite3.connect('sajth.db')
 c = db.cursor()
 
 #c.execute("""CREATE TABLE sajith(firstname text,lastname text,age int) """)
-c.execute("INSERT INTO sajith VALUES('vishnu', 'mohan','20')")
-
+#c.execute("INSERT INTO sajith VALUES('vishnu', 'mohan','20')")
+rows = c.execute("SELECT * FROM sajith")
 db.commit()
 #close the cursor
+for row in rows:
+    print(row)
+
+
 db.close()
